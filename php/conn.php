101a14ce71d770c93788f0cbb3f333267a464215
adrijshikhar <adrijshikhar85@gmail.com>
2019-06-26 16:31:13 +0530

    fixed conn.php

--- php/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = 'localhost';
-$username = 'evem';
-$password = 'evem123';
-$db = 'evem';
-$conn = new mysqli($servername, $username, $password, $db);
-if ($conn->error) {
-    die("connection_failed" . $conn->connect_error);
-}
